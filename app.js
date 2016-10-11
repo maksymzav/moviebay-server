7529e98ddd891c6038142efe432c921ba8c8499b
Motsyak Vasyl <neformen@gmail.com>
2016-10-12 00:43:11 +0300

    update api

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('./films', films);
+app.use('/films', films);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
